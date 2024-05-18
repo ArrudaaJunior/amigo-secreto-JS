e102e4a2a0a462e98d333111b9b2090f7e2e3d84
Arruda Junior <juniorarruda15@gmail.com>
2024-05-18 14:36:34 -0300

    Função para embaralha o array de amigos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let listaAmigos = document.getElementById('lista-amigos');
-    
+    amigos.push(amigo.value);
+
     if (listaAmigos.textContent == '') {
         listaAmigos.textContent = amigo.value;
     } else {
@@ -11,7 +14,20 @@ function adicionar() {
 }
 
 function sortear() {
+    embaralha(amigos);
+    
+}
 
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
 
 function reiniciar() {
