aca736ffd683f61c2ebb10895ac97cb5fbee1697
Arruda Junior <juniorarruda15@gmail.com>
2024-05-18 14:12:28 -0300

    Adiconado um nome na lista

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let listaAmigos = document.getElementById('lista-amigos');
+    
+    if (listaAmigos.textContent == '') {
+        listaAmigos.textContent = amigo.value;
+    } else {
+        listaAmigos.textContent = listaAmigos.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+}
+
+function sortear() {
+
+}
+
+function reiniciar() {
+
+}
